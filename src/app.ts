3b1f77126f70151e732c055fc548df9d70427fd6
SALEH <salehenab850@gmail.com>
2024-07-07 13:56:28 +0300

    Make the code more readable

--- src/app.ts
+++ src/app.ts
@@ -15,30 +15,32 @@ app.get("/", (req, res) => {
 })
 
 
-app.post("/register", [check("fullname")
-    .matches(/^[^\d]*$/)
-    .withMessage("Full name must not contain any numbers"),
-check("email")
-    .isEmail()
-    .normalizeEmail()
-    .withMessage("Invalid Email"),
-check("BD").isDate({ format: "DD/MM/YYYY" })
-    .withMessage("Invalid Date"), check("password")
-        .matches(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,64}$/)
-        .withMessage("Week Password"),
-check("password2")
-    .custom((value, { req }) => { return value === req.body.password })
-    .withMessage("Passwords do not match")], (req: Request, res: Response) => {
+app.post("/register", [
+    check("fullname")
+        .matches(/^[^\d]*$/)
+        .withMessage("Full name must not contain any numbers"),
+    check("email")
+        .isEmail()
+        .normalizeEmail()
+        .withMessage("Invalid Email"),
+    check("BD").isDate({ format: "DD/MM/YYYY" })
+        .withMessage("Invalid Date"), check("password")
+            .matches(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,64}$/)
+            .withMessage("Week Password"),
+    check("password2")
+        .custom((value, { req }) => { return value === req.body.password })
+        .withMessage("Passwords do not match")
+], (req: Request, res: Response) => {
 
-        const errors = validationResult(req)
-        if (!errors.isEmpty()) {
-            const errorsArr = errors.array()
-            res.render('index', { errors: errorsArr })
-        } else {
-            res.send("SUCCESSFUL")
-        }
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        const errorsArr = errors.array()
+        res.render('index', { errors: errorsArr })
+    } else {
+        res.send("SUCCESSFUL")
+    }
 
-    })
+})
 const port = process.env.PORT || 5000
 app.listen(port, () => {
     console.log("Connceted Successfully...")
